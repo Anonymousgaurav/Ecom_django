22a7283a195144ccb92eece4892daae2dfbaa6f4
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-01-01 22:37:54 +0530

    categories sidebar bootstrap added

--- store/views.py
+++ store/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models.product import Product
+from .models.product import Product, Category
 
 
 # Create your views here.
 
 def dummydata(request):
     products = Product.get_all_products()
-    return render(request, 'index.html', {'products': products})
+    categories = Category.get_all_categories()
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+    # return render(request, 'index.html', {'products': products}, {'categories': categories})
+    return render(request, 'index.html', data)
+
+
+1
