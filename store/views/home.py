77ebad3f2dc3f4f33869e25b5ce445a3a1de3506
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-01-20 23:13:26 +0530

    passing id while adding on add to cart button

--- store/views/home.py
+++ store/views/home.py
@@ -6,21 +6,30 @@ from store.models import Product, Category
 from store.models import Customer
 from django.contrib.auth.hashers import make_password, check_password
 from django.views import View
+from django.views import View
 
 
 # Create your views here
-def homepage(request):
-    products = None
-    categories = Category.get_all_categories()
-    categoryID = request.GET.get('category')
-    if categoryID:
-        products = Product.get_all_products_by_id(categoryID)
-    else:
-        products = Product.get_all_products()
 
-    data = {}
-    data['products'] = products
-    data['categories'] = categories
-    # return render(request, 'index.html', {'products': products}, {'categories': categories})
-    print('You are :', request.session.get('customer_email'))
-    return render(request, 'index.html', data)
+
+class Index(View):
+
+    def post(self, request):
+        product = request.POST.get('product')
+        return redirect('homepage')
+
+    def get(self, request):
+        products = None
+        categories = Category.get_all_categories()
+        categoryID = request.GET.get('category')
+        if categoryID:
+            products = Product.get_all_products_by_id(categoryID)
+        else:
+            products = Product.get_all_products()
+
+        data = {}
+        data['products'] = products
+        data['categories'] = categories
+        # return render(request, 'index.html', {'products': products}, {'categories': categories})
+        print('You are :', request.session.get('customer_email'))
+        return render(request, 'index.html', data)
