5dc4498127a2ef619305a59c5c25162f3d174973
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-01-23 22:36:50 +0530

    cart page and functionality added

--- store/urls.py
+++ store/urls.py
@@ -3,10 +3,14 @@ from django.urls import path
 from store.views import login, signup, home
 from .views.home import Index
 from .views.login import logout
+from .views.cart import Cart
 
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
     path('signup', signup.SignUp.as_view(), name='signup'),
     path('login', login.Login.as_view(), name='login'),
-    path('logout', logout, name='logout')
+    path('logout', logout, name='logout'),
+    path('cart', Cart.as_view(), name='cart')
 ]
+
+
