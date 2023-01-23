5a8319c1b886926b1fdd122c114c66890ea80366
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-01-23 14:52:44 +0530

    logout functionality added

--- store/urls.py
+++ store/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from store.views import login, signup, home
 from .views.home import Index
+from .views.login import logout
 
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
     path('signup', signup.SignUp.as_view(), name='signup'),
-    path('login', login.Login.as_view(), name='login')
+    path('login', login.Login.as_view(), name='login'),
+    path('logout', logout, name='logout')
 ]
