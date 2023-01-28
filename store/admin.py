e050164a0fa9a0d2305bfdf901457d208bcd52d2
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-01-28 09:34:19 +0530

    orders table created for checkout page

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Product, Category
 from .models.customer import Customer
+from .models.orders import Order
 
 
 # Register your models here.
@@ -16,3 +17,4 @@ class AdminCategory(admin.ModelAdmin):
 admin.site.register(Product, AdminProduct)
 admin.site.register(Category, AdminCategory)
 admin.site.register(Customer)
+admin.site.register(Order)
