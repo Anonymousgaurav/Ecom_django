bf68d47161296aa3fec4cfdd72a4c5213ffc89f4
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-01-29 17:27:23 +0530

    order status added to orders page

--- store/models/orders.py
+++ store/models/orders.py
@@ -12,6 +12,7 @@ class Order(models.Model):
     address = models.CharField(max_length=50, default="", blank=True)
     phone = models.CharField(max_length=10, default="", blank=True)
     date = models.DateField(default=datetime.datetime.today)
+    delivery = models.BooleanField(default=False)
 
     @staticmethod
     def get_orders_by_customer(customer_id):
