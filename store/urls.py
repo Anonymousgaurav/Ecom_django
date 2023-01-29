dea51460d2355901bb955062c92619b38ce8f882
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-01-29 16:40:29 +0530

    home screen cart quantity increment and decrememt functionality added

--- store/urls.py
+++ store/urls.py
@@ -4,13 +4,15 @@ from store.views import login, signup, home
 from .views.home import Index
 from .views.login import logout
 from .views.cart import Cart
+from .views.checkout import Checkout
 
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
     path('signup', signup.SignUp.as_view(), name='signup'),
     path('login', login.Login.as_view(), name='login'),
     path('logout', logout, name='logout'),
-    path('cart', Cart.as_view(), name='cart')
+    path('cart', Cart.as_view(), name='cart'),
+    path('check-out', Checkout.as_view(), name='checkout')
 ]
 
 
